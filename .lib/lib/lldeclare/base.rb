d332f74158d5507373dc7dc410e77be122996292
Ryosuke IWANAGA <riywo.jp@gmail.com>
2012-12-28 01:44:23 -0800

    set_env

--- .lib/lib/lldeclare/base.rb
+++ .lib/lib/lldeclare/base.rb
@@ -10,19 +10,29 @@ class LLDeclare::Base
   end
 
   def exec(command)
+    set_env
     Kernel.exec(shell(command))
   end
 
 private
 
+  def set_env
+    home = ENV["HOME"]
+    ENV.clear
+    ENV["HOME"] = home
+    ENV["SHELL"] = "/bin/bash"
+  end
+
   def shell(command)
   end
 
   def system(command)
+    set_env
     Kernel.system(shell(command))
   end
 
   def backtick(command)
+    set_env
     `#{shell(command)}`
   end
 
